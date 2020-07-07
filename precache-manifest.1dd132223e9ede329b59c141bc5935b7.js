1669975ac92daaaadff1dec61925ed6de4a3469f
Oshinka <oshinka94@gmail.com>
2020-07-07 12:54:03 +0530

    Updates

---
+++ precache-manifest.1dd132223e9ede329b59c141bc5935b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd54b7665fa6f9be3cc65306f68e56f9",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "1fec09051c1777260651",
+    "url": "/covid-19/static/css/main.22207a5b.chunk.css"
+  },
+  {
+    "revision": "af634b27e6b880b63ce8",
+    "url": "/covid-19/static/js/2.7f91b883.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19/static/js/2.7f91b883.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fec09051c1777260651",
+    "url": "/covid-19/static/js/main.1b1d6b99.chunk.js"
+  },
+  {
+    "revision": "a614627b5e5a8bb0690a",
+    "url": "/covid-19/static/js/runtime-main.33977434.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid-19/static/media/logo.5d5d9eef.svg"
+  }
+]);
