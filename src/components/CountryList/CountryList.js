19942b37050d445cd9bc1ca89c63a19d538383f2
Oshinka <oshinka94@gmail.com>
2020-07-07 12:47:28 +0530

    saving files into git hub

---
+++ src/components/CountryList/CountryList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Country from '../Country/Country'
+import './CountryList.css'
+
+const CountryList = ({stats}) => {
+    return (
+        <div className='countryList'>
+            {
+                stats.map(country => <Country key={country.CountryCode} stats={country}/>)
+            }
+        </div>
+    )
+}
+export default CountryList;
